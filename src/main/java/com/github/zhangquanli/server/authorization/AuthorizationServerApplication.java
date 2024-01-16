a0d37030d3a0fbbdab0ea41ef284d21407a0609d
zhangquanli <39901255+zhangquanli@users.noreply.github.com>
2024-01-16 17:22:34 +0800

    Update AuthorizationServerApplication.java

--- src/main/java/com/github/zhangquanli/server/authorization/AuthorizationServerApplication.java
+++ src/main/java/com/github/zhangquanli/server/authorization/AuthorizationServerApplication.java
@@ -1,8 +1,12 @@
 package com.github.zhangquanli.server.authorization;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
+@Slf4j
 @SpringBootApplication
 public class AuthorizationServerApplication {
 
