b0199f69fe2ea3d95d9866fdabab903878c8c135
allen <allen.ql.zhang@cn.ey.com>
2024-01-17 09:56:55 +0800

    更新部署文件

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-noup java -jar /java/oauth2-authorization-server.jar --jasypt.encryptor.password=$JASYPT_ENCRYPTOR_PASSWORD > /dev/null &
+noup java -jar /java/oauth2-authorization-server.jar --jasypt.encryptor.password=$JASYPT_ENCRYPTOR_PASSWORD  &
